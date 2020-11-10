dbae65395f0570217fd5e74a756f074dfd22544b
federico mateucci <federicomateucci@gmail.com>
2020-11-10 14:31:18 -0300

    Tp5-Colores-VueCli

---
+++ src/components/MainColors.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="container">
+			<Square 
+				class="square" 
+        :index="index"
+				:fmensajes="fmensajes"
+				:mensajes="mensajes"
+				:square="square"
+				:fsquare="fsquare"
+				:set-all-colors-to="setAllColorsTo"
+				:picked-color="pickedColor"
+				v-for="(square,index) in squares"
+				:key="index"
+			/>
+		</div>
+
+</template>
+
+<script>
+
+  import Square from './Square'
+
+  export default  {
+    name: 'src-components-main-colors',
+    props: [
+      'squares','fsquare',
+      'mensajes','fmensajes',
+      'pickedColor',
+      'setAllColorsTo'
+    ],
+    components: {
+      Square
+    },
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-main-colors {
+
+  }
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,48 @@
+<template>
+    <div :style="square" @click="squareClick()"></div>
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-square',
+    props: [
+      'index',
+      'square','fsquare', 
+      'mensajes','fmensajes',
+      'pickedColor',
+      'setAllColorsTo'
+    ],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+      squareClick(){
+        var clickedColor = this.square.backgroundColor;
+        if (clickedColor === this.pickedColor) {
+          this.fmensajes.messageDisplay("You Picked Right!");
+          this.setAllColorsTo(this.pickedColor);
+          this.fmensajes.restartButton("Play Again!");
+          //this.mensajes.restartButton = "Play Again!";  // --> NO!
+          this.fmensajes.headerColor(this.pickedColor);
+        } else {
+          this.fsquare.backgroundColor(this.index,"#232323");
+          //this.square.backgroundColor = "#232323"; // --> NO!
+          this.fmensajes.messageDisplay("Try Again!");
+        }
+      }
+    },
+    computed: {
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+</style>
